20bd1ca2fdb8cb67002cda5e8444abcee5d7e12a
Leonardo Araujo <leo.san9@hotmail.com>
2022-05-17 14:55:42 -0300

    Delete post ok

--- src/components/postCard/postCard.js
+++ src/components/postCard/postCard.js
@@ -1,10 +1,34 @@
 import "./post-style.css"
+import toast, {Toaster} from "react-hot-toast"
+
 export function PostCard(props) {
+    function Reload(){
+        window.location.reload()
+    }
+    async function HandleDelete(){
+        try{
+            await props.api.delete(`/post/delete-post/${props.id}`)
+            toast.success('Post deleted') 
+            console.log(props.id)
+            await setTimeout(Reload, 1000)
+        } catch(err){
+            toast.error('Can not delete the post, try again later') 
+            console.log(err)
+        }     
+    }
+    
     return ( 
         <div className="Post">
+        <div>   
+            <Toaster
+            position="top-center"
+            reverseOrder={false}
+            />
+        </div>
             <div className="PostHeader">
                 <img src={`${props.ProfileImg}`} alt={`${props.UserName}ProfileImage`} className="PostProfile"/>
                 <h5>{props.UserName}</h5>
+                <button onClick={HandleDelete}>Delete</button>
                 
             </div>
             <div>
@@ -15,3 +39,4 @@ export function PostCard(props) {
         </div>
      );
 }
+
