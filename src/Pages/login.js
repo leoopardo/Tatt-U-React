165e16b5d4dc08518046c234680658babf28ce93
Leonardo Araujo <leo.san9@hotmail.com>
2022-05-20 03:23:55 -0300

    agenda, n sei como fazer :(

--- src/Pages/login.js
+++ src/Pages/login.js
@@ -14,7 +14,7 @@ export function Login() {
     });
     const navigate = useNavigate()
     const {setLoggedInUser} = useContext(AuthContext)
-    const { loggedInUser } = useContext(AuthContext)
+    
 
     function handleChange(e){
         setLogin({...login, [e.target.name]: e.target.value})
