4bd733a9456a415d885f1591b5dd0e0f322699ea
Leonardo Araujo <leo.san9@hotmail.com>
2022-05-20 11:27:45 -0300

    react calendar

--- src/Pages/schedule.js
+++ src/Pages/schedule.js
@@ -1,8 +1,18 @@
 import { NavBarSimple } from "../components/navsBar/navBarSimple";
-export function  Schedule() {
+import { useState } from "react";
+import Calendar from "react-calendar/dist/umd/Calendar";
+import { AuthContext } from "../contexts/authContext";
+import { useContext } from "react";
+import 'react-calendar/dist/Calendar.css';
 
+export function  Schedule() {
+    const [value, onChange] = useState(new Date());
+    const {loggedInUser} = useContext(AuthContext)
  
     return ( <div className="feed">
-        <NavBarSimple/>
+        <NavBarSimple>
+            <img src={loggedInUser.user.profilePicture} alt="profile_pic" className="profilePic"/>
+        </NavBarSimple>
+        <Calendar onChange={onChange} value={value}/>
     </div> );
 }
